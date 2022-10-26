461abfb9433d4d451e23d71efe5f6ca74ce3dc98
Santiagitk10 <93751091+Santiagitk10@users.noreply.github.com>
2022-10-26 16:39:29 -0500

    notes

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -19,3 +19,10 @@ export class UiService {
     return this.subject.asObservable();
   }
 }
+
+
+//Este servicio usa un Subject y Suscription para que otros componentes estén escuchando cuando
+//pasa algo, en este caso cuando se hace click al botón de add/Close. Cuando eso pasa se 
+//cambia el valor booleando de la propiedad ShowAddTask y se suscriben los componentes add-task
+//y header en sus constructores para recibir el valor booleando y mostrar o no sus 
+//componentes o para cambiar el color y texto del botón.  
